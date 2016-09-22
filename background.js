39dd2537ecbd8ddd371106194479c023f24b5f76
Vasco Silva <vasco.silva@tekzenit.com>
2016-09-22 21:56:04 +0100

    First commit, basic functionality and style

---
+++ background.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2016 Vasco Silva
+var active = false;
+
+function updateIcon() {
+  active = !active;
+
+  if (active) {
+    chrome.browserAction.setIcon({path:"icon2.png"});
+    chrome.tabs.executeScript({
+      code:"document.designMode = 'on';", allFrames: true
+    });
+
+  } else {
+    chrome.browserAction.setIcon({path:"icon1.png"});
+    chrome.tabs.executeScript({
+      code:"document.designMode = 'off';", allFrames: true
+    });
+  }
+}
+
+chrome.browserAction.onClicked.addListener(updateIcon);
